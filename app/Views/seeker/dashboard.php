9111b2ee627fbf5046c940d81ed12cd008e98565
KUUUUe <chansavin90@gmail.com>
2026-03-24 11:17:51 +0700

    som 1

--- app/Views/seeker/dashboard.php
+++ app/Views/seeker/dashboard.php
@@ -1,7 +1,7 @@
 <?php require BASE_PATH . '/app/Views/layouts/header.php'; ?>
 <section><div class="container"><div class="text-center">
      <h1>My Dashboard</h1><br>
-     <p class="lead">KUKUKAKA 126 <?php echo htmlspecialchars($_SESSION['email']); ?>!</p>
+     <p class="lead">KUKUKAKA 126 ssssss <?php echo htmlspecialchars($_SESSION['email']); ?>!</p>
      <br>
      <a href="<?php echo $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']; ?>/jobs" class="section-btn btn btn-default">Browse Jobs</a>
 </div></div></section>
