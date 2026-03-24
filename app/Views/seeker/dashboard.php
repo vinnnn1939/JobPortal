a44d7830707a7736c3445e5de94f68ba7169db18
KUUUUe <chansavin90@gmail.com>
2026-03-24 11:22:39 +0700

    Update

--- app/Views/seeker/dashboard.php
+++ app/Views/seeker/dashboard.php
@@ -6,3 +6,4 @@
      <a href="<?php echo $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']; ?>/jobs" class="section-btn btn btn-default">Browse Jobs</a>
 </div></div></section>
 <?php require BASE_PATH . '/app/Views/layouts/footer.php'; ?>
+<h1>HII</h1>
